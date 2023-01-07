a46182ea8704a5541e2b2553fd88be6301aa9e5c
ocassidydev <ocassidy@tcd.ie>
2023-01-07 00:51:40 +0000

    Add toggle published status of recipe in admin panel

--- recipes/admin.py
+++ recipes/admin.py
@@ -7,8 +7,12 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Recipe)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'author', 'slug', 'created_on', 'status')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title', )}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
+    actions = ['publish_recipe']
+
+    def publish_recipe(self, request, queryset):
+        queryset.update(status=1)
