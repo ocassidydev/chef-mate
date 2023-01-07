79030b6df84400df999d28f43938f14c54dc0fc2
ocassidydev <ocassidy@tcd.ie>
2023-01-07 00:22:50 +0000

    Add summernote package to manage admin panel display of db items

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Recipe
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
-admin.site.register(Recipe)
+@admin.register(Recipe)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
