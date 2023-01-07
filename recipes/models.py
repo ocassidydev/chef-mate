5b0865d0d6de88c59e051a8191c0393bacdd1893
ocassidydev <ocassidy@tcd.ie>
2023-01-07 00:59:15 +0000

    Add some defaults to Recipe model to make admin panel for adding recipes more useable

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 STATUS = ((0, "Unpublished"), (1, "Published"))
 
@@ -10,14 +11,16 @@ class Recipe(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="recipes")
+                               related_name="recipes", default='admin')
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     likes = models.ManyToManyField(User, related_name='recipe_likes',
                                    blank=True)
-    created_on = models.DateTimeField()
+    # possible issue - this causes the time submitted to be when the form
+    # is opened, not when it's submitted
+    created_on = models.DateTimeField(default=timezone.now)
     updated_on = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS)
+    status = models.IntegerField(choices=STATUS, default='Unpublished')
 
     class Meta:
         ordering = ['-created_on']
