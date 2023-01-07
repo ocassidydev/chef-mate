f806c6a677b64f2e716f763f39d65280990a99f7
ocassidydev <ocassidy@tcd.ie>
2023-01-07 00:29:09 +0000

    Fix issue with list_filter in admin.py

--- recipes/admin.py
+++ recipes/admin.py
@@ -10,5 +10,5 @@ class PostAdmin(SummernoteModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title', )}
-    list_filter = {'status', 'created_on'}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
